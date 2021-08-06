7b8d512a9d6b1d8b09707ab2b604151cc6621b78
Kieran Porter <kieranintehdas@gmail.com>
2021-08-07 00:11:55 +0100

    Add validation on meal plan end date

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,4 +1,12 @@
 class MealPlan < ApplicationRecord
   has_many :meal_plan_meals
   has_many :meals, through: :meal_plan_meals
+
+  validate :end_after_start?
+
+  def end_after_start?
+    if end_date < start_date
+      errors.add(:end_date, "must be after start date")
+    end
+  end
 end
