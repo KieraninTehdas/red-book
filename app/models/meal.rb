2c4b46a2a47c09ccced587d28975e696c42b2355
Kieran Porter <kieranintehdas@gmail.com>
2021-08-08 22:55:39 +0100

    Finally fix pesky meal validation

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,8 @@ class Meal < ApplicationRecord
   belongs_to :recipe_book, optional: true
   has_many :meal_plan_meals
   has_many :meal_plans, through: :meal_plan_meals
-  validates :page_number, absence: true, numericality: { only_integer: true, allow_nil: true }, unless: -> { recipe_book.present? }
+  validates :page_number, numericality: { only_integer: true, allow_nil: true }
+  validates :recipe_book, presence: true, if: -> { page_number.present? }
 
   def recipe_book_name
     recipe_book&.name
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -7,9 +7,15 @@ RSpec.describe Meal, type: :model do
     end
   end
 
-  context 'when page number is set without recipe book' do
-    it 'is not saved successfully' do
-      expect(Meal.new({name: 'Name', page_number: 55}).save).to be false
+  context 'recipe book is not set' do
+    context 'and page number is set' do
+      it 'is not saved successfully' do
+        expect(Meal.new({name: 'Name', page_number: 55}).save).to be false
+      end
+    end
+
+    it 'returns nill when recipe_book_name is called' do
+      expect(Meal.new({name: 'Name'}).recipe_book_name).to be_nil
     end
   end
 
@@ -24,10 +30,29 @@ RSpec.describe Meal, type: :model do
   context 'when recipe book is set' do
     let(:recipe_book) { create(:recipe_book) }
 
+    context 'and page number is not set' do
+      it 'is saved successfully' do
+        expect(Meal.new({name: 'Name', recipe_book: recipe_book}).save).to be true
+      end
+    end
+
     context 'and page number is non-numeric' do
       it 'is not saved successfully' do
         expect(Meal.new({name: 'Name', recipe_book: recipe_book, page_number: 'wc3f'}).save).to be false
       end
     end
+
+    context 'and page number is not an integer' do
+      it 'is not saved successfully' do
+        expect(Meal.new({name: 'Name', recipe_book: recipe_book, page_number: '15.353'}).save).to be false
+      end
+    end
+
+    it 'recipe_book_name returns the correct name' do
+      meal = Meal.new({name: 'Name', recipe_book: recipe_book}) 
+      
+      expect(meal.recipe_book_name).to eq recipe_book.name
+    end
   end
 end
+
