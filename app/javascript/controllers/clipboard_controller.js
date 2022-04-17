5f6319f4c1cea3bce437be0bc8fb8abf80780248
Kieran Porter <kieranintehdas@gmail.com>
2022-04-17 21:50:51 +0100

    Add copy to clipboard button for shopping lists

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["source", "alertPlaceholder"];
+
+  copy() {
+    navigator.clipboard.writeText(this.sourceTarget.value);
+
+    if (!!this.alertPlaceholderTarget) {
+      this.showAlert("List copied to clipboard!", "success");
+    }
+  }
+
+  showAlert(message, type) {
+    if (this.alertPlaceholderTarget.childElementCount > 0) {
+      return;
+    }
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML =
+      '<div class="alert alert-' +
+      type +
+      ' alert-dismissible" role="alert">' +
+      message +
+      '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+    this.alertPlaceholderTarget.append(wrapper);
+  }
+}
--- app/javascript/controllers/populate_input_controller.js
+++ app/javascript/controllers/populate_input_controller.js
@@ -2,17 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = { url: String };
-  static targets = ["input"];
+  static targets = ["input", "inputContainer"];
 
   populateInput() {
-    console.log(this.urlValue);
-
     fetch(this.urlValue, {
       headers: { "Content-Type": "application/json" },
     }).then((response) => {
       response.json().then((body) => {
         this.inputTarget.value = body;
-        this.inputTarget.style.display = "inline";
+        this.inputContainerTarget.style.display = "inline";
       });
     });
   }
